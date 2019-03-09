6fd8ee1dbcb420e39a945e8b40522e48e63aa40b
oussou-dev <oussou.dev@gmail.com>
2019-03-09 17:07:17 +0000

    Add components

---
+++ src/components/movie-list/MovieElement.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+
+class MovieElement extends Component {
+    render() {
+        return(
+            <div>
+                Movie Element
+            </div>
+        )
+    }
+}
+
+export default MovieElement
