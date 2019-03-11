a13db72a1b346eb5f963be8776918b6f20e7d2d6
oussou-dev <oussou.dev@gmail.com>
2019-03-11 10:29:15 +0000

    Refactorisation

--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -3,12 +3,13 @@ import React, { Component } from "react"
 class MovieElement extends Component {
     render() {
         return(
-            <div className="w-25 p-2">
-                <div className="card">
-                    <img alt="film" src="https://is1-ssl.mzstatic.com/image/thumb/Video71/v4/85/1c/82/851c823f-271e-877f-21ef-766b29f3a3a6/mzm.gjakfclu.lsr/268x0w.png" className="card-img-top"/>
-                    <div classNAme="card-body">
-                        <h5 className="card-title"> GLADIATOR</h5>
-                        <p className="card-text"> Sed ut perspiciatis unde omnis iste natus error sit
+            <div className="w-50 p-2">
+                <div className="border d-flex">
+                    <img width="150" height="200" alt="film" src="https://is1-ssl.mzstatic.com/image/thumb/Video71/v4/85/1c/82/851c823f-271e-877f-21ef-766b29f3a3a6/mzm.gjakfclu.lsr/268x0w.png"/>
+                    <div className="flex-fill d-flex flex-column p-3">
+                        <h5> Titre</h5>
+                        <hr className="w-100"/>
+                        <p> Sed ut perspiciatis unde omnis iste natus error sit
                         voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
                         ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
                         </p>
